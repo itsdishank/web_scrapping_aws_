dd752a8b4e317af57b1d27fac0e5cdb69f073ba6
itsdishank <dishuhanwate007@gmail.com>
2023-03-01 13:19:40 +0530

    made changes to og file

--- application.py
+++ application.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import pymongo
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+import csv
 
 application = Flask(__name__) # initializing a flask app
 app=application
@@ -11,78 +15,93 @@ app=application
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
-    return render_template("index.html")
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
-@cross_origin()
-def index():
-    if request.method == 'POST':
-        try:
-            searchString = request.form['content'].replace(" ","")
-            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
-            uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+    driver.get("https://www.youtube.com/@PW-Foundation/videos")
+    driver.execute_script("window.scrollBy(0, 10000);")
 
-            filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+    yt_html = driver.page_source
 
-                except:
-                    name = 'No Name'
+    yt_soup = bs(yt_html,"html.parser")
 
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
+    # try:
 
+    video_urls = yt_soup.find_all('a',{"class":"yt-simple-endpoint inline-block style-scope ytd-thumbnail"})
 
-                except:
-                    rating = 'No Rating'
+    urls_list = []
 
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
+    for i in video_urls[1:6]:
+        urls_list.append("https://www.youtube.com"+i['href'])
 
-                except:
-                    commentHead = 'No Comment Heading'
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    print("Exception while creating dictionary: ",e)
+    thumbnail_url = yt_soup.find_all('img',{'class':"yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded"})
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
-            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
-        except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-    # return render_template('results.html')
+    image_urls_list = []
 
-    else:
-        return render_template('index.html')
+    for image_url in thumbnail_url[0:5]:
+        image_urls_list.append(image_url['src'])
+
+    video_titles = yt_soup.find_all('a',{'class':'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'})
+
+    video_title_list = []
+
+    for video_title in (video_titles[0:5]):
+        video_title_list.append(video_title.text)
+
+    video_views = yt_soup.find_all('span',{'class':'inline-metadata-item style-scope ytd-video-meta-block'})
+
+    count = 0
+    video_views_list = []
+
+    for views in video_views:
+        if count >=5:
+            break
+        else:
+            if re.search("^[0-9].*views$",views.text):
+                video_views_list.append(views.text)
+                count+=1
+
+    uploaded_time = yt_soup.find_all('span',{'class':'inline-metadata-item style-scope ytd-video-meta-block'})
+
+    count = 0
+    uploaded_time_list = []
+
+    for time in uploaded_time:
+        if count >=5:
+            break
+        else:
+            if re.search("[0-9].*ago$",time.text):
+                uploaded_time_list.append(time.text)
+                count+=1
+
+    # sample dictionary with keys as column names and values as lists
+    my_dict = {'URL': urls_list,
+            'Image URL': image_urls_list,
+            'Video Title': video_title_list,
+            'Views': video_views_list,
+            'time': uploaded_time_list
+            }
+    my_dict_list = []
+
+    # open a new CSV file for writing with DictWriter
+    with open('my_file.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=my_dict.keys())
+
+        # write the header row with the column names
+        writer.writeheader()
+
+        # write the data rows with values from the dictionary
+        for i in range(len(my_dict['URL'])):
+            try:
+                row_data = {key: my_dict[key][i] for key in my_dict.keys()}
+            except:
+                print('Exception Occured')
+            else:
+                my_dict_list.append(row_data)
+                writer.writerow(row_data)
+    # except:
+    #     print("Cannot find in list")
+
+    return render_template('results.html', my_dict_list=my_dict_list)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
